55e451ba3b2f1691721fad900b2cd83fe6e75a44
MarcDavid99 <marcdavid99@gmail.com>
2019-03-19 20:14:42 +0200

    testin kas git töötab mul

--- src/Deadline.java
+++ src/Deadline.java
@@ -2,4 +2,6 @@ public class Deadline {
     //peaks tegema ka deadline klassi, kus siis meetodid deadlinei settimiseks, gettimiseks jne
     //mingi by default deadline võiks ka olla iga ülesande lisamisel ja siis äkki mingi meetod mis ütleks
     //kas taski deadline on möödas
+
+    //testin kas git töötab.
 }
